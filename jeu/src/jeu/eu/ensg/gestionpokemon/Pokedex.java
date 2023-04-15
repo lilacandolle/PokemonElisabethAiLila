28c2ee8f584ef905ea678fb5baa85259c75dab15
aiheula <ai-net@legouix.net>
2023-04-15 10:34:15 +0200

    add pokedex

---
+++ jeu/src/jeu/eu/ensg/gestionpokemon/Pokedex.java
@@ -0,0 +1,14 @@
+package gestionpokemon
+
+public class Pokedex {
+    private type fichier ; 
+    //charger données pokémons
+    public charger(type fichier) {
+        load fichier ;
+        pokeList = []
+        for (i : fichier.name) {
+            pokeList += new Pokemon(fichier.name, fichier.type, fichier.hp)
+        }
+    }
+
+}
