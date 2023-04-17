6241b75a9ae8d607da07771b500c1105eb9c5aba
lilacandolle <lila.cassan@gmail.com>
2023-04-17 12:11:08 +0200

    j'ai ajouté les classes coord, famille et mdj

---
+++ jeu/src/jeu/eu/ensg/gestiondespokemons/Coordinate.java
@@ -0,0 +1,25 @@
+package jeu.eu.ensg.gestiondespokemons;
+
+public class Coordinate {
+    //créer coordinate avec x, y comme attributs, des getters, et une distance
+    private int x;
+    private int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Coordinate c) {
+        return Math.sqrt(Math.pow(this.x - c.getX(), 2) + Math.pow(this.y - c.getY(), 2));
+    }
+    
+}
---
+++ jeu/src/jeu/eu/ensg/gestiondespokemons/Famille.java
@@ -0,0 +1,6 @@
+package jeu.eu.ensg.gestiondespokemons;
+
+public enum Famille {
+    //créer énumération des familles de pokémons
+    FAMILLE, COMBAT, DRAGON, EAU, FEU, ELECTRIK, FEE, GLACE, INSECTE, NORMAL, PLANTE, POISSON, PSY, ROCHE, SOL, SPECTRE, TENEBRES, VOL
+}
