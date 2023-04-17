aaa98890f193c1cce8b49a34b7ab176c386ecc2a
aiheula <ai-net@legouix.net>
2023-04-17 16:42:55 +0200

    La ca doit marcher par pitie

--- jeu/src/jeu/eu/ensg/gestionDesPokemons/Combat.java
+++ jeu/src/jeu/eu/ensg/gestionDesPokemons/Combat.java
@@ -1,23 +1,41 @@
 package gestiondespokemons ;
 
 public class Combat {
-    private Pokemon pokemonS = //pokemon choisit par le joueur ;
+    private Pokemon pokemonS = ; //!pokemon choisit par le joueur ;
     private Pokemon pokemonD ;
     private int hpS = pokemonS.getHP() ;
     private int hpD = pokemonD.getHP() ;
     
     public changerPok() {
         pokemonD = clique(pokemonMDJ) ;
+        System.out.println("Tu as choisis " + pokemonD)
     }
 
     public attaque(typAtk, Pokemon PA, Pokemon DD) {
         Famille famA = getFamille(PA) ;
-        Famille famD = getFamille(PD) ;
-        if typAtk
+        Famille famD = getFamille(DD) ;
+        if (typAtk == NEUTRE) {
+            degats = 20 }
+        else {
+            degats = (int) coef * 20 }
+        
+        if (PA == pokemonS) {
+            hpD -= degats 
+            System.out.println(pokemonS.getPokemon + " a infligé " + degats + " dégâts à " + pokemonD.getPokemon)
+        }
+        else {
+            hpS -= degats 
+            System.out.println(pokemonD.getPokemon + " a infligé " + degats + " dégâts à " + pokemonS.getPokemon)
+        }
+        
+        System.out.println(pokemonS.getPokemon + " a " + hpS + " HP !")
+        System.out.println(pokemonD.getPokemon + " a " + hpD + " HP !")
+
     }
 
     public fuite() {
-        //quitte le combat
+        //!quitte le combat
+        System.out.println("Tu t'enfuies !")
     }
 
     public static void main(String[] args) {
@@ -28,8 +46,6 @@ public class Combat {
         while (hpS>0 && hpD>0) {
 
             if (i % 2 = 0) {
-                PA = pokemonD ;
-                DD = PokemonS ;
 
                 //Le joueur clique sur une option
                 if (clique = fuite) {
@@ -37,11 +53,11 @@ public class Combat {
                 }
 
                 if (clique = attaque_neutre) {
-                    attaque(neutre, PA, DD)
+                    attaque(neutre, pokemonD, pokemonS)
                 }
 
                 if (clique = attaque_element) {
-                    attaque(element, PA, DD)
+                    attaque(element, pokemonD, pokemonS)
                 }
 
                 if (clique = changerPok) {
@@ -53,10 +69,10 @@ public class Combat {
                 PA = pokemonS ;
                 DD = pokemonD ;
                 if (random > 0.5) {
-                    attaque(neutre, PA, DD)
+                    attaque(neutre, pokemon, pokemonD)
                 }
                 else {
-                    attaque(element, PA, DD)
+                    attaque(element, pokemonS, pokemonD)
                 }
             }
 
