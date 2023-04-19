48f9c03393e514814b6cc00b01ef49117fa97774
lilacandolle <lila.cassan@gmail.com>
2023-04-19 15:45:16 +0200

    chargerpokemon

--- jeu/src/jeu/eu/ensg/Main/ChargerPoke.java
+++ jeu/src/jeu/eu/ensg/Main/ChargerPoke.java
@@ -4,25 +4,42 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import gestionDesPokemons.Famille;
+import gestionDesPokemons.Pokemon;
 
 public class ChargerPoke {
+    
     public static void main(String[] args) {
 
-        String pathFile = "/PokemonElisabethAiLila/data/pokemon_first_gen.csv";
-        String line = " " ;
+        String csvFile = "/Users/lila.cassan/Desktop/PokemonElisabethAiLila/data/pokemon_first_gen.csv";
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(pathFile)) ;
-            while((line = br.readLine()) != null)
-                System.out.println(line) ;
-            System.out.println("File found");
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            String[] headers = reader.readNext(); // ignorer la première ligne qui contient les en-têtes
+
+            List<Pokemon> pokemons = new ArrayList<>();
+
+            Object[] line;
+            while ((line = reader.readNext()) != null) {
+                String nom = (String) line[0];
+                Famille famille = (Famille.fromString((String) line[1]));
+                int HP = (int)line[4];
+
+                Pokemon pokemon = new Pokemon(nom, famille, HP);
+                pokemons.add(pokemon);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CsvValidationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace() ;
-            System.out.println("File not found");
-        }
-        catch (IOException e) {
-            e.printStackTrace() ;
-        }
+
     }
 }
